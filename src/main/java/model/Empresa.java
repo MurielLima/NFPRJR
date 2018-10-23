e04870a09bb906b8be0c14c91b68451d5a797325
Muriel Lima <muriellima2@gmail.com>
2018-10-23 21:16:36 -0200

    add pom

--- src/main/java/model/Empresa.java
+++ src/main/java/model/Empresa.java
@@ -5,10 +5,84 @@
  */
 package model;
 
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
+import org.springframework.data.mongodb.core.mapping.DBRef;
+import org.springframework.data.mongodb.core.mapping.Document;
+
 /**
  *
  * @author Muriel
  */
+@Document
 public class Empresa {
+    @Id
+    private String idEmpresa;
+    @Indexed(unique=true)
+    private String cnpj;
+    private String nomeFantasia;
+    private String razaoSocial;
+    @DBRef
+    private Cidade cidade;
+
+    public Empresa() {
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 53 * hash + (this.idEmpresa != null ? this.idEmpresa.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Empresa other = (Empresa) obj;
+        if ((this.idEmpresa == null) ? (other.idEmpresa != null) : !this.idEmpresa.equals(other.idEmpresa)) {
+            return false;
+        }
+        return true;
+    }
+
+    public String getCnpj() {
+        return cnpj;
+    }
+
+    public void setCnpj(String cnpj) {
+        this.cnpj = cnpj;
+    }
+
+    public String getNomeFantasia() {
+        return nomeFantasia;
+    }
+
+    public void setNomeFantasia(String nomeFantasia) {
+        this.nomeFantasia = nomeFantasia;
+    }
+
+    public String getRazaoSocial() {
+        return razaoSocial;
+    }
+
+    public void setRazaoSocial(String razaoSocial) {
+        this.razaoSocial = razaoSocial;
+    }
+
+    public Cidade getCidade() {
+        return cidade;
+    }
+
+    public void setCidade(Cidade cidade) {
+        this.cidade = cidade;
+    }
     
 }
