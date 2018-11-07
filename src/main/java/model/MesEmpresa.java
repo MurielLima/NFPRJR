48e37112b4edf60ef8184d465532b61251ef0e41
bueno <bueno.030702@gmail.com>
2018-11-07 18:36:52 -0200

    Tab Pane Meses/Empresa listener

--- src/main/java/model/MesEmpresa.java
+++ src/main/java/model/MesEmpresa.java
@@ -65,8 +65,22 @@ public class MesEmpresa {
     }
 
     public String getMes() {
-        return mes;
-    }
+        switch (mes){
+            case ("01") : return "Janeiro";        
+            case ("02")  : return "Fevereiro";
+            case ("03")  : return "Março";
+            case ("04")  : return "Abril";
+            case ("05")  : return "Maio";
+            case ("06")  : return "Junho";
+            case ("07")  : return "Julho";
+            case ("08")  : return "Agosto";
+            case ("09")  : return "Setembro";
+            case ("10")  : return "Outubro";
+            case ("11")  : return "Novembro";
+            case ("12")  : return "Dezembro";
+            
+    }   return mes;
+}
 
     public void setMes(String mes) {
         this.mes = mes;
--- src/main/java/model/Meses.java
+++ src/main/java/model/Meses.java
@@ -46,7 +46,21 @@ public class Meses {
     }
 
     public String getMes() {
-        return mes;
+                switch (mes){
+            case ("01") : return "Janeiro";        
+            case ("02")  : return "Fevereiro";
+            case ("03")  : return "Março";
+            case ("04")  : return "Abril";
+            case ("05")  : return "Maio";
+            case ("06")  : return "Junho";
+            case ("07")  : return "Julho";
+            case ("08")  : return "Agosto";
+            case ("09")  : return "Setembro";
+            case ("10")  : return "Outubro";
+            case ("11")  : return "Novembro";
+            case ("12")  : return "Dezembro";
+            
+    }   return mes;
     }
 
     public void setMes(String mes) {
@@ -87,7 +101,7 @@ public class Meses {
 
     @Override
     public String toString() {
-        return mes +  "/" + ano;
+        return getMes() +  "/" + ano;
     }
     
 
